cbd640f395a6ef4fd5e248924191d4f82d4aafc3
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-03-23 20:47:40 +0000

    Colocacion de Torre en un lugar determinado

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,8 +6,8 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Sprite imagenTorre;
-    public Image imagenBoton;
+    public Image imagenTorre;
+    public GameObject boton;
     void Start()
     {
         
@@ -19,6 +20,13 @@ public class GameManager : MonoBehaviour
     }
     public void crearTorre()
     {
-        imagenBoton.GetComponent<Image>().sprite = imagenTorre.GetComponent<Image>().sprite;
+        boton.GetComponent<Image>().sprite = imagenTorre.sprite;
+        // 1. Pillamos los RectTransform de ambos
+        RectTransform rectTorre = imagenTorre.GetComponent<RectTransform>();
+        RectTransform rectBoton = boton.GetComponent<RectTransform>();
+
+        // 2. Igualamos el ancho y el alto directamente
+        rectBoton.sizeDelta = new Vector2(rectTorre.rect.width, rectTorre.rect.height);
+        boton.GetComponentInChildren<TextMeshProUGUI>().text = "";
     }
 }
