e11bd0d554fe56a4aeac55f18fcb668d90abc641
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-04-20 20:01:23 +0100

    A

---
+++ Assets/deleteTower.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class deleteTower : MonoBehaviour
+{
+    public bool isDeleteTower = false;
+    private void OnMouseDown()
+    {
+        isDeleteTower = true;
+    }
+}
