7131be032e7d5def40795866f344ba2fe26616a4
carluimirher <carluimirher@gmail.com>
2026-04-29 17:19:31 +0100

    Cierre del ConstruccionMenu al abrir UpdgradeMenu en torres

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -167,6 +167,7 @@ public class Tower : MonoBehaviour
 
         towerActiveInMenu = this;
         setGameObjectUpDeleStatus(true);
+        constructionMenu.cancelFunction();
         assignInformationImage();
         assignInformationText();
 
