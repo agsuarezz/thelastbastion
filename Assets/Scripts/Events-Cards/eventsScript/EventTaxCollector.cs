0a44301ae5aa2e9508048af4911647e32ed246ac
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-05-16 15:57:55 +0100

    Ajuste en TaxCollector para tener mas de un evento de ese tipo

--- Assets/Scripts/Events-Cards/eventsScript/EventTaxCollector.cs
+++ Assets/Scripts/Events-Cards/eventsScript/EventTaxCollector.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
 using System.Collections;
+
 [CreateAssetMenu(fileName = "NewTaxCollector", menuName = "Bastion/Events/Tax Collector")]
 public class EventTaxCollector : DynamicEvent
 {
+    [Header("Configuración del Robo")]
+    [Tooltip("Cantidad de oro EXACTA que te quita. Pon 0 si prefieres usar el multiplicador.")]
+    public int fixedGoldToRemove = 0;
+
+    [Tooltip("Porcentaje de oro que quita (1 = 100%, 0.5 = 50%). Solo funciona si fixedGoldToRemove es 0.")]
+    public float theftMultiplier = 1f;
+
     public override IEnumerator Execute()
     {
-        GameManager.countMoney = 0;
+        // Si le hemos puesto un valor fijo (ej: 1 moneda), quitamos eso
+        if (fixedGoldToRemove > 0)
+        {
+            GameManager.countMoney -= fixedGoldToRemove;
+        }
+        // Si no, usamos el porcentaje (ej: 1f para quitarlo todo)
+        else
+        {
+            int taxes = Mathf.RoundToInt(GameManager.countMoney * theftMultiplier);
+            GameManager.countMoney -= taxes;
+        }
+
         yield return new WaitForSeconds(5f);
     }
 }
