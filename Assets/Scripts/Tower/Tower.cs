00d038cf73f4cfdf8c31c9784bf7ec630fddf427
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-05-18 15:08:05 +0100

    Elimine algunas librerias que no se utilizaban ya que tiraban error al hacer build and run

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,9 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.U2D;
 using UnityEngine.UI;
 
 /// <summary>
