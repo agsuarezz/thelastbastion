d3f4d31abc25a948c1b4970193d7c54ef5c7b378
Marcos González Gómez <marcosgonzalezgomez9@gmail.com>
2026-05-20 19:07:53 +0100

    boss2 sound added

--- Assets/Scripts/Enemys/EnemyBlockTilesOnDeath.cs
+++ Assets/Scripts/Enemys/EnemyBlockTilesOnDeath.cs
@@ -10,8 +10,21 @@ public class EnemyBlockTilesOnDeath : MonoBehaviour
     public int roundsDuration = 3;
     public float blockRadius = 3f;
 
+    [Header("Sonido")]
+    public AudioClip fireSpawnSound;
+    [Range(0f, 1f)]
+    public float soundVolume = 1f;
+
     public void BlockTilesOnDeath()
     {
+        if (fireSpawnSound != null)
+{
+    AudioSource.PlayClipAtPoint(
+        fireSpawnSound,
+        transform.position,
+        soundVolume
+    );
+}
         int tilesBlocked = 0;
         int attempts = 0;
 
@@ -54,6 +67,7 @@ public class EnemyBlockTilesOnDeath : MonoBehaviour
                 gridPosition,
                 Quaternion.identity
             );
+            
 
             BlockedTile blockedTile = block.GetComponent<BlockedTile>();
 
